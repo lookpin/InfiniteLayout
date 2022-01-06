a7cf7362ce068ffb813b384527cca5d4357ab6bb
MyunggiSon <myunggi.son@lookpin.co.kr>
2022-01-06 16:04:38 +0900

    Use RxSwift 6.0.0 and RxDataSources 5.0.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["RxInfiniteLayout"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.1.1")),
-        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "4.0.1")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.2")),
     ],
     targets: [
         .target(
